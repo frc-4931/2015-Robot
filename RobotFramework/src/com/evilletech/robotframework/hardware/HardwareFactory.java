695b55e073ff185a6d98c26aef34f91b745692d9
Zach Anderson <Zach@speakinwebs.com>
2014-11-18 16:21:53 -0600

    Created the robot componet factories

---
+++ RobotFramework/src/com/evilletech/robotframework/hardware/HardwareFactory.java
@@ -0,0 +1,14 @@
+package com.evilletech.robotframework.hardware;
+
+
+/**
+ * The factory for all devices that directly interface with robot hardware.
+ * <code>HardwareFactory</code> provides factory methods for all physical
+ * hardware components.
+ * 
+ * @author Zach Anderson
+ * 
+ */
+public class HardwareFactory {
+
+}
---
+++ RobotFramework/src/com/evilletech/robotframework/mockhardware/MockFactory.java
@@ -0,0 +1,15 @@
+package com.evilletech.robotframework.mockhardware;
+
+
+/**
+ * The factory for all mock hardware devices. A mock hardware device allows an
+ * element of the robot to be simulated independent of the rest of the hardware
+ * that would be required. <code>MockFactory</code> provides a factory method
+ * for every initial state of all mock hardware devices.
+ * 
+ * @author Zach Anderson
+ * 
+ */
+public class MockFactory {
+
+}
