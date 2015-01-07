1ce1485be1c895b1f13e3d799978205d5ec06a11
AGausmann <adam.gausmann@gmail.com>
2015-01-06 21:39:08 -0600

    Prototype Ramp subsystem - Topic open for debate: Solenoid vs LimitedMotor for guardrails?

---
+++ RobotFramework/src/org/frc4931/robot/subsystem/Ramp.java
@@ -0,0 +1,72 @@
+/*
+ * FRC 4931 (http://www.evilletech.com)
+ *
+ * Open source software. Licensed under the FIRST BSD license file in the
+ * root directory of this project's Git repository.
+ */
+
+package org.frc4931.robot.subsystem;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import org.frc4931.robot.component.LimitedMotor;
+import org.frc4931.robot.component.Solenoid;
+
+import java.util.function.Supplier;
+
+public final class Ramp extends Subsystem {
+    public static final double RAIL_MOVE_SPEED = 0.0d;
+
+    private final Solenoid lifter;
+    private final LimitedMotor guardRail; // Debatable - solenoid vs motor?
+    private final Supplier<Command> defaultCommandSupplier;
+
+    /**
+     * Creates a new Ramp subsystem using a Solenoid for the lifting mechanism and a LimitedMotor for the guardrails.
+     *
+     * @param lifter The lifting mechanism; may not be null.
+     * @param guardRail The guard rail control mechanism; may not be null.
+     * @param defaultCommandSupplier The supplier for this subsystem's default command; may be null if there is no default command.
+     */
+    public Ramp(Solenoid lifter, LimitedMotor guardRail, Supplier<Command> defaultCommandSupplier) {
+        this.lifter = lifter;
+        this.guardRail = guardRail;
+        this.defaultCommandSupplier = defaultCommandSupplier;
+    }
+
+    /**
+     * Raises the ramp using the lifting mechanism.
+     */
+    public void raise() {
+        lifter.extend();
+    }
+
+    /**
+     * Lowers the ramp using the lifting mechanism.
+     */
+    public void lower() {
+        lifter.retract();
+    }
+
+    /**
+     * Opens the ramp's guard rails, releasing the held objects.
+     */
+    public void openGuardRail() {
+        guardRail.moveTowardsHigh(RAIL_MOVE_SPEED);
+    }
+
+    /**
+     * Closes the ramp's guard rails, securing the held objects.
+     */
+    public void closeGuardRail() {
+        guardRail.moveTowardsLow(RAIL_MOVE_SPEED);
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+        if ( defaultCommandSupplier != null ) {
+            Command command = defaultCommandSupplier.get();
+            setDefaultCommand(command);
+        }
+    }
+}
