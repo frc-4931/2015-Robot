63aaed48af09932e1b0b546f037fea7519bb5ac5
Colin Hauch <colin.hauch@gmail.com>
2015-01-11 17:57:31 -0600

    Created command stubs

---
+++ RobotFramework/src/org/frc4931/robot/command/CloseGrabber.java
@@ -0,0 +1,31 @@
+/*
+ * FRC 4931 (http://www.evilletech.com)
+ *
+ * Open source software. Licensed under the FIRST BSD license file in the
+ * root directory of this project's Git repository.
+ */
+package org.frc4931.robot.command;
+
+/**
+ * Closes the grabber arm.
+ */
+public class CloseGrabber extends CommandBase {
+
+    @Override
+    protected void initialize() {
+    }
+
+    @Override
+    protected void execute() {
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    protected void end() {
+    }
+
+}
---
+++ RobotFramework/src/org/frc4931/robot/command/LiftStack.java
@@ -0,0 +1,32 @@
+/*
+ * FRC 4931 (http://www.evilletech.com)
+ *
+ * Open source software. Licensed under the FIRST BSD license file in the
+ * root directory of this project's Git repository.
+ */
+package org.frc4931.robot.command;
+
+/**
+ * Lifts the whole stack of totes further up the
+ * ramp.
+ */
+public class LiftStack extends CommandBase {
+
+    @Override
+    protected void initialize() {
+    }
+
+    @Override
+    protected void execute() {
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    protected void end() {
+    }
+
+}
