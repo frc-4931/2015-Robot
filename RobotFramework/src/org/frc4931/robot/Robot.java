0dd5fd39d2073cd90ded7d0c43834aacef9fa436
Zach Anderson <zach@speakinwebs.com>
2015-01-11 16:56:56 -0600

    Created robot builder to instanciate robot componets

--- RobotFramework/src/org/frc4931/robot/Robot.java
+++ RobotFramework/src/org/frc4931/robot/Robot.java
@@ -6,11 +6,21 @@
  */
 package org.frc4931.robot;
 
+import org.frc4931.robot.subsystem.DriveSystem;
+import org.frc4931.robot.subsystem.LoaderArm;
+import org.frc4931.robot.subsystem.Ramp;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class Robot extends IterativeRobot {
     @Override
     public void robotInit() {
 
+    }
+    
+    public static final class Componets {
+        public static DriveSystem drive;
+        public static LoaderArm grabber;
+        public static Ramp ramp;
     }
 }
