e4f5ba0173cff4600c54998d232dc052c9e4cdac
Nathan Brown <nbrown735@yahoo.com>
2015-01-17 12:19:25 -0600

    worked on testOpenGrabber program

---
+++ RobotFramework/testsrc/org/frc4931/robot/command/OpenGrabberTest.java
@@ -0,0 +1,38 @@
+/*
+ * FRC 4931 (http://www.evilletech.com)
+ * 
+ * Open source software. Licensed under the FIRST BSD license file in the
+ * root directory of this project's Git repository.
+ */
+package org.frc4931.robot.command;
+
+import org.frc4931.robot.Robot.Systems;
+import org.junit.Test;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class OpenGrabberTest extends AbstractCommandTest {
+
+    @Override
+    protected Command createCommand(Systems systems) {
+        return new CloseGrabber(systems.grabber);
+    }
+    
+    @Test
+    public void shouldCloseGrabberWhenGrabberIsOpen() {
+        // Preconditions ...
+        robot.grabberActuator().extend();
+        robot.assertGrabberSolenoidExtended();
+        // Create and execute the command multiple times ...
+        repeat(10,()->runCommandAnd(robot::assertGrabberSolenoidRetracted));
+    }
+        
+    @Test
+    public void shouldCloseGrabberWhenGrabberIsAlreadyClosed() {
+        // Preconditions ...
+        robot.grabberActuator().retract();
+        robot.assertGrabberSolenoidRetracted();
+        // Create and execute the command multiple times ...
+        repeat(10,()->runCommandAnd(robot::assertGrabberSolenoidRetracted));
+    }
+}
