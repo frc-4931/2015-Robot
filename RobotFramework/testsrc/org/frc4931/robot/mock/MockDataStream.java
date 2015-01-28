e94f6472779ed4f77fde38a47843249623f3cd1e
AGausmann <adam.gausmann@gmail.com>
2015-01-27 19:51:32 -0600

    Implemented MockDataStream

--- RobotFramework/testsrc/org/frc4931/robot/mock/MockDataStream.java
+++ RobotFramework/testsrc/org/frc4931/robot/mock/MockDataStream.java
@@ -8,16 +8,19 @@ package org.frc4931.robot.mock;
 
 import org.frc4931.robot.component.DataStream;
 
+import java.nio.ByteBuffer;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class MockDataStream implements DataStream {
     private Queue<Byte> readData;
     private Queue<Byte> writtenData;
+    private Queue<Byte> flushedData;
 
     public MockDataStream() {
         readData = new PriorityQueue<>();
         writtenData = new PriorityQueue<>();
+        flushedData = new PriorityQueue<>();
     }
 
     public void addReadData(byte... data) {
@@ -26,94 +29,218 @@ public class MockDataStream implements DataStream {
         }
     }
 
-    public byte[] getWrittenData(int count) {
-        byte[] data = new byte[Math.min(count, writtenData.size())];
+    public void addByte(byte value) {
+        addReadData(value);
+    }
+
+    public void addShort(short value) {
+        ByteBuffer buffer = ByteBuffer.allocate(Short.BYTES);
+        buffer.putShort(value);
+        buffer.flip();
+
+        addReadData(buffer.array());
+    }
+
+    public void addInt(int value) {
+        ByteBuffer buffer = ByteBuffer.allocate(Integer.BYTES);
+        buffer.putInt(value);
+        buffer.flip();
+
+        addReadData(buffer.array());
+    }
+
+    public void addLong(long value) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(value);
+        buffer.flip();
+
+        addReadData(buffer.array());
+    }
+
+    public void addFloat(float value) {
+        ByteBuffer buffer = ByteBuffer.allocate(Float.BYTES);
+        buffer.putFloat(value);
+        buffer.flip();
+
+        addReadData(buffer.array());
+    }
+
+    public void addDouble(double value) {
+        ByteBuffer buffer = ByteBuffer.allocate(Double.BYTES);
+        buffer.putDouble(value);
+        buffer.flip();
+
+        addReadData(buffer.array());
+    }
+
+    public byte[] getFlushedData(int count) {
+        byte[] data = new byte[Math.min(count, flushedData.size())];
 
         for (int i = 0; i < data.length; i++) {
-            data[i] = writtenData.remove();
+            data[i] = flushedData.remove();
         }
 
         return data;
     }
 
+    public byte getByte() {
+        byte[] bytes = getFlushedData(Byte.BYTES);
+
+        return ByteBuffer.wrap(bytes).get();
+    }
+
+    public short getShort() {
+        byte[] bytes = getFlushedData(Short.BYTES);
+
+        return ByteBuffer.wrap(bytes).get();
+    }
+
+    public int getInt() {
+        byte[] bytes = getFlushedData(Integer.BYTES);
+
+        return ByteBuffer.wrap(bytes).get();
+    }
+
+    public long getLong() {
+        byte[] bytes = getFlushedData(Long.BYTES);
+
+        return ByteBuffer.wrap(bytes).get();
+    }
+
+    public float getFloat() {
+        byte[] bytes = getFlushedData(Float.BYTES);
+
+        return ByteBuffer.wrap(bytes).get();
+    }
+
+    public double getDouble() {
+        byte[] bytes = getFlushedData(Double.BYTES);
+
+        return ByteBuffer.wrap(bytes).get();
+    }
+
     @Override
     public int available() {
-        return 0;
+        return readData.size();
     }
 
     @Override
     public byte[] read(int count) {
-        return new byte[0];
+        byte[] result = new byte[Math.max(count, readData.size())];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = readData.remove();
+        }
+        return result;
     }
 
     @Override
     public byte readByte() {
-        return 0;
+        byte[] bytes = read(Byte.BYTES);
+
+        return ByteBuffer.wrap(bytes).get();
     }
 
     @Override
     public short readShort() {
-        return 0;
+        byte[] bytes = read(Short.BYTES);
+
+        return ByteBuffer.wrap(bytes).getShort();
     }
 
     @Override
     public int readInt() {
-        return 0;
+        byte[] bytes = read(Integer.BYTES);
+
+        return ByteBuffer.wrap(bytes).getInt();
     }
 
     @Override
     public long readLong() {
-        return 0;
+        byte[] bytes = read(Long.BYTES);
+
+        return ByteBuffer.wrap(bytes).getLong();
     }
 
     @Override
     public float readFloat() {
-        return 0;
+        byte[] bytes = read(Float.BYTES);
+
+        return ByteBuffer.wrap(bytes).getFloat();
     }
 
     @Override
     public double readDouble() {
-        return 0;
+        byte[] bytes = read(Double.BYTES);
+
+        return ByteBuffer.wrap(bytes).getDouble();
     }
 
     @Override
     public int write(byte... data) {
-        return 0;
+        int writeCount = 0;
+        for (byte b : data) {
+            if (writtenData.offer(b)) {
+                writeCount++;
+            }
+        }
+
+        return writeCount;
     }
 
     @Override
     public void writeByte(byte value) {
-
+        write(value);
     }
 
     @Override
     public void writeShort(short value) {
+        ByteBuffer buffer = ByteBuffer.allocate(Short.BYTES);
+        buffer.putShort(value);
+        buffer.flip();
 
+        write(buffer.array());
     }
 
     @Override
     public void writeInt(int value) {
+        ByteBuffer buffer = ByteBuffer.allocate(Integer.BYTES);
+        buffer.putInt(value);
+        buffer.flip();
 
+        write(buffer.array());
     }
 
     @Override
     public void writeLong(long value) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(value);
+        buffer.flip();
 
+        write(buffer.array());
     }
 
     @Override
     public void writeFloat(float value) {
+        ByteBuffer buffer = ByteBuffer.allocate(Float.BYTES);
+        buffer.putFloat(value);
+        buffer.flip();
 
+        write(buffer.array());
     }
 
     @Override
     public void writeDouble(double value) {
+        ByteBuffer buffer = ByteBuffer.allocate(Double.BYTES);
+        buffer.putDouble(value);
+        buffer.flip();
 
+        write(buffer.array());
     }
 
     @Override
     public void flush() {
-
+        flushedData.addAll(writtenData);
+        writtenData.clear();
     }
 
     @Override
@@ -123,6 +250,8 @@ public class MockDataStream implements DataStream {
 
     @Override
     public void shutdown() {
-
+        readData.clear();
+        writtenData.clear();
+        flushedData.clear();
     }
 }
