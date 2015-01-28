5dbe11084a03db64d480826ad629dbb3f7d3d057
AGausmann <adam.gausmann@gmail.com>
2015-01-28 17:13:05 -0600

    Moved RIODuino to the robot.hardware package

--- RobotFramework/src/org/frc4931/robot/hardware/HardwareRIODuino.java
+++ RobotFramework/src/org/frc4931/robot/hardware/HardwareRIODuino.java
@@ -4,20 +4,21 @@
  * Open source software. Licensed under the FIRST BSD license file in the
  * root directory of this project's Git repository.
  */
-package org.frc4931.robot.component;
+package org.frc4931.robot.hardware;
 
 import edu.wpi.first.wpilibj.SerialPort;
+import org.frc4931.robot.component.SerialDataStream;
 
 /**
  * A component representing a RIODuino, an Arduino connected to the roboRIO via MXP.
  */
-public final class RIODuino extends SerialDataStream {
+public final class HardwareRIODuino extends SerialDataStream {
     /**
      * Create a new connection over I2C to the RIODuino.
      *
      * @param baudRate The rate at which to send data over the serial port.
      */
-    public RIODuino(int baudRate) {
+    public HardwareRIODuino(int baudRate) {
         super(new SerialPort(baudRate, SerialPort.Port.kMXP));
     }
 }
