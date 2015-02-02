1836698c2330031a47a0944a569be96f08eb48de
Zach Anderson <zach@speakinwebs.com>
2015-02-02 15:20:27 -0600

    Initial scheduler framework

---
+++ RobotFramework/src/org/frc4931/robot/command/Scheduler.java
@@ -0,0 +1,51 @@
+/*
+ * FRC 4931 (http://www.evilletech.com)
+ *
+ * Open source software. Licensed under the FIRST BSD license file in the
+ * root directory of this project's Git repository.
+ */
+package org.frc4931.robot.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ */
+public class Scheduler {
+    private static final Scheduler INSTANCE = new Scheduler();
+    public static Scheduler getInstance() { return INSTANCE; }
+    
+    private final Commands list = new Commands();
+    
+    /**
+     * Schedule a {@link Command} to be added to the {@link Scheduler}.
+     * @param command the {@link Command} to be added
+     */
+    public void add(Command command) {
+        list.add(command);
+    }
+    
+    /**
+     * Runs once though all of the {@link Command}s in the {@link Scheduler}.
+     */
+    public void run() {
+        list.step();
+    }
+    
+    private final static class Commands {
+        List<CommandRunner> beingExecuted = new ArrayList<>();
+        List<CommandRunner> pendingAdditon = new ArrayList<>();
+        
+        public Commands() { }
+
+        public void step() {
+            beingExecuted.addAll(pendingAdditon);
+            beingExecuted.forEach((runner)->step());
+        }
+        
+        public void add(Command command) {
+            pendingAdditon.add(new CommandRunner(command));
+        }
+    }
+}
