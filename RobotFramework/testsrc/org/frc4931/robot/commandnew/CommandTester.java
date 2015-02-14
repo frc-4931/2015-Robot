050752ddc4b1b3717d86c8378ffef01484605357
Zach Anderson <zach@speakinwebs.com>
2015-02-14 16:20:34 -0600

    Created wrapper to test commands

---
+++ RobotFramework/testsrc/org/frc4931/robot/commandnew/CommandTester.java
@@ -0,0 +1,17 @@
+/*
+ * FRC 4931 (http://www.evilletech.com)
+ *
+ * Open source software. Licensed under the FIRST BSD license file in the
+ * root directory of this project's Git repository.
+ */
+package org.frc4931.robot.commandnew;
+
+
+/**
+ * 
+ */
+public class CommandTester extends CommandRunner{
+    public CommandTester(Command command) {
+        super(command);
+    }
+}
