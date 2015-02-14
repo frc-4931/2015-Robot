05752a7ad9016bf9d601fac03b363aaf469905af
Zach Anderson <zach@speakinwebs.com>
2015-02-14 14:45:54 -0600

    Removed second header in 'LoaderArm'

--- RobotFramework/src/org/frc4931/robot/subsystem/LoaderArm.java
+++ RobotFramework/src/org/frc4931/robot/subsystem/LoaderArm.java
@@ -5,13 +5,6 @@
  * root directory of this project's Git repository.
  */
 
-/*
- * FRC 4931 (http://www.evilletech.com)
- *
- * Open source software. Licensed under the FIRST BSD license file in the
- * root directory of this project's Git repository.
- */
-
 package org.frc4931.robot.subsystem;
 
 import java.util.function.Supplier;
