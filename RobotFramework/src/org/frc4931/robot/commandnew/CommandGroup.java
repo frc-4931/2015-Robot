30580547e29ec57589a61e8274fb7fd7721f3e14
Zach Anderson <zach@speakinwebs.com>
2015-02-14 10:15:29 -0600

    Added documentation to 'CommandGroup'

--- RobotFramework/src/org/frc4931/robot/commandnew/CommandGroup.java
+++ RobotFramework/src/org/frc4931/robot/commandnew/CommandGroup.java
@@ -8,6 +8,45 @@ package org.frc4931.robot.commandnew;
 
 import org.frc4931.robot.commandnew.Scheduler.Commands;
 
+/**
+ * <p>
+ * A {@link CommandGroup} is a series of {@link Command}s executed either in sequence or in
+ * parallel that must happen in a certain order. A command group is created by extending
+ * {@link CommandGroup}. In the constructor of the subclass should be one call to either
+ * {@link #sequentially(Command...)} or {@link #simultaneously(Command...)}. In the first
+ * call can be any combination of {@link Command}s, {@link #sequentially(Command...)}s,
+ * {@link #simultaneously(Command...)}s, and {@link #fork(Command)}.
+ * </p><p>
+ * 
+ * <li>In a {@link #sequentially(Command...)} call the next {@link Command} starts when the
+ * previous has ended. The whole block finishes when the final {@link Command} is done.
+ * 
+ * <li>In a {@link #simultaneously(Command...)} call all of the {@link Command}s are
+ * executed at the same time. The whole block does not finish until the last {@link Command}
+ * is done.
+ * 
+ * <li>A {@link #fork(Command)} call should contain either one {@link Command} or one of the
+ * above two. when {@link #fork(Command)} is called, everything inside of it is executed
+ * independently of the rest of the {@link CommandGroup}. For the purposes of
+ * {@link #sequentially(Command...)} the block finishes instantly.
+ * 
+ * </p><br><p>
+ * The constructor of a {@link CommandGroup} that executes three {@link Command}s in order:<br>
+ * <code>sequentially(c1, c2, c3);</code>
+ * </p><br>
+ * 
+ * <p>
+ * The constructor of a {@link CommandGroup} that executes three {@link Command}s at the
+ * same time:<br>
+ * <code>simultaneously(c1, c2, c3);</code>
+ * </p><br>
+ * 
+ * <p>
+ * The constructor of a {@link CommandGroup} that executes two {@link Command}s at the
+ * same time and then a third {@link Command} after both are finished:<br>
+ * <code>sequentially(simultaneously(c1, c2), c3);</code>
+ * </p>
+ */
 public class CommandGroup extends Command{
     private Command root;
     private final Command[] commands;
