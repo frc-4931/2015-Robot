7237d6ba42286ab657009c1e5ebbccbb6b1fe2c2
Zach Anderson <zach@speakinwebs.com>
2015-03-03 15:51:27 -0600

    Migrated 'Robot' over to the new framework

--- RobotFramework/src/org/frc4931/robot/Robot.java
+++ RobotFramework/src/org/frc4931/robot/Robot.java
@@ -6,90 +6,125 @@
  */
 package org.frc4931.robot;
 
+import org.frc4931.robot.commandnew.Scheduler;
+import org.frc4931.robot.commandnew.grabber.ToggleGrabber;
+import org.frc4931.robot.commandnew.grabber.ToggleGrabberLift;
+import org.frc4931.robot.commandnew.guardrail.ToggleGuardrail;
+import org.frc4931.robot.commandnew.kicker.MoveKickerToGround;
+import org.frc4931.robot.commandnew.kicker.MoveKickerToGuardrail;
+import org.frc4931.robot.commandnew.kicker.MoveKickerToTransfer;
+import org.frc4931.robot.commandnew.ramplifter.ToggleRamp;
 import org.frc4931.robot.component.DataStream;
 import org.frc4931.robot.component.Motor;
 import org.frc4931.robot.component.MotorWithAngle;
 import org.frc4931.robot.component.Relay;
 import org.frc4931.robot.component.Solenoid;
 import org.frc4931.robot.component.Switch;
-import org.frc4931.robot.driver.OperatorInterface;
 import org.frc4931.robot.subsystem.DriveSystem;
 import org.frc4931.robot.subsystem.LoaderArm;
 import org.frc4931.robot.subsystem.Ramp;
 import org.frc4931.robot.subsystem.StackIndicatorLight;
 import org.frc4931.robot.subsystem.VisionSystem;
+import org.frc4931.robot.system.RobotBuilder;
+import org.frc4931.robot.system.RobotSystems;
 import org.frc4931.utils.Lifecycle;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
 public class Robot extends IterativeRobot {
+    private static final long START_TIME = System.nanoTime();
     public static final int NUMBER_OF_ADC_BITS = 12;
-    private static Robot instance;
-    private static long startTime = System.nanoTime();
-    private Systems systems;
-    private OperatorInterface operatorInterface;
+    
+    private static RobotSystems robot;
 
-    public Systems getSubsystems() {
-        return systems;
+    public RobotSystems get() {
+        return robot;
     }
 
     @Override
     public void robotInit() {
-        instance = this;
+        robot = RobotBuilder.buildRobot();
+        
+        SwitchListener.getInstance().onTriggered(robot.operator.toggleLift, ()->Scheduler.getInstance().add(new ToggleGrabberLift(robot.structure.grabber)));
+        SwitchListener.getInstance().onTriggered(robot.operator.toggleClaw, ()->Scheduler.getInstance().add(new ToggleGrabber(robot.structure.grabber)));
+        SwitchListener.getInstance().onTriggered(robot.operator.toggleRails, ()->Scheduler.getInstance().add(new ToggleGuardrail(robot.structure.ramp.rail)));
+        SwitchListener.getInstance().onTriggered(robot.operator.toggleRamp, ()->Scheduler.getInstance().add(new ToggleRamp(robot.structure.ramp.lifter)));
+        
+        SwitchListener.getInstance().onTriggered(robot.operator.kickerToGround, ()->Scheduler.getInstance().add(new MoveKickerToGround(robot.structure.kickerSystem.kicker)));
+        SwitchListener.getInstance().onTriggered(robot.operator.kickerToTransfer, ()->Scheduler.getInstance().add(new MoveKickerToTransfer(robot.structure.kickerSystem.kicker)));
+        SwitchListener.getInstance().onTriggered(robot.operator.kickerToGuardrail, ()->Scheduler.getInstance().add(new MoveKickerToGuardrail(robot.structure.kickerSystem.kicker)));
+       
+        SwitchListener.getInstance().onTriggered(robot.operator.writeData, Logger.getInstance()::shutdown);
+        SwitchListener.getInstance().onTriggered(robot.operator.writeData, ()->System.out.println("DATA SAVED"));
+        
         SwitchListener.getInstance().start();
-        Components components = RobotBuilder.components();
-        systems = RobotBuilder.build(components);
-        operatorInterface = RobotBuilder.operatorInterface();
-        PowerDistributionPanel pdp = new PowerDistributionPanel();
-        Logger.getInstance().registerSwitch("Arm lowered", components.armLifterLowerSwitch());
-        Logger.getInstance().registerSwitch("Arm raised", components.armLifterUpperSwitch());
-        Logger.getInstance().registerSwitch("Capturable", components.capturableSwitch());
-        Logger.getInstance().registerSwitch("Captured", components.capturedSwitch());
-        Logger.getInstance().registerSwitch("Guardrail opened", components.guardRailOpenSwitch());
-        Logger.getInstance().registerSwitch("Guardrail closed", components.guardRailClosedSwitch());
-        Logger.getInstance().register("Kicker motor angle", () -> (short) (components.kickerMotor().getAngle()) * 1000);
-        Logger.getInstance().register("Right drive motor speed", ()->components.rightDriveMotor().getSpeedAsShort());
-        Logger.getInstance().register("Left drive motor speed", ()->components.leftDriveMotor().getSpeedAsShort());
-        Logger.getInstance().register("Arm motor speed", ()->components.armLifterActuator().getSpeedAsShort());
-        Logger.getInstance().register("Guardrail motor speed", ()->components.guardRailActuator().getSpeedAsShort());
-        Logger.getInstance().register("Channel 15 Current", () -> (short) (pdp.getCurrent(15) * 1000));
-        Logger.getInstance().register("Channel 14 Current", () -> (short) (pdp.getCurrent(14) * 1000));
+
+        Logger.getInstance().register("Drive Speed", ()->(int)(robot.operator.driveSpeed.read()*1000));
+        Logger.getInstance().register("Turn Speed",  ()->(int)(robot.operator.turnSpeed.read()*1000));
+        Logger.getInstance().register("Throttle",    ()->(int)(robot.operator.throttle.read()*1000));
+        
+        Logger.getInstance().register("Guardrail Status",     ()->robot.componets.guardrailGrabber.isExtending() ? 1 : 0);
+        Logger.getInstance().register("Left Grabber Status",  ()->robot.componets.grabberLeftGrabber.isExtending() ? 1 : 0);
+        Logger.getInstance().register("Right Grabber Status", ()->robot.componets.grabberRightGrabber.isExtending() ? 1 : 0);
+        Logger.getInstance().register("Ramp Lifter Status",   ()->robot.componets.rampLifter.isExtending() ? 1 : 0);
+        
+        Logger.getInstance().register("Kicker Position", ()->(int)(robot.componets.kickerMotor.getAngleSensor().getAngle()*1000));
+        Logger.getInstance().register("Kicker Current",  ()->(int)(robot.componets.kickerMotor.getCurrentSensor().getCurrent()*1000));
+        
+        Logger.getInstance().register("Grabber Position", ()->(int)(robot.componets.grabberEncoder.getAngle()*1000));
+        
+        Logger.getInstance().register("X Accel", ()->(int)(robot.componets.builtInAccel.getXacceleration()*1000));
+        Logger.getInstance().register("Y Accel", ()->(int)(robot.componets.builtInAccel.getYacceleration()*1000));
+        Logger.getInstance().register("Z Accel", ()->(int)(robot.componets.builtInAccel.getZacceleration()*1000));
+        
         Logger.getInstance().startup();
-        // Start each of the subsystems and other objects that need initializing ...
-        systems.startup();
+    }
+    
+    public void activePeriodic() {
+        Scheduler.getInstance().step(time());
+    }
+    
+    @Override
+    public void disabledInit() {
+        Scheduler.getInstance().killAll();
+    }
+    
+    // Active code starts here
+    @Override
+    public void autonomousInit() {
+        
+    }
+    
+    @Override
+    public void autonomousPeriodic() {
+        activePeriodic();
+    }
+    
+    @Override
+    public void teleopInit() {
+        robot.structure.ramp.lifter.lower();
+        robot.structure.ramp.rail.open();
+        robot.structure.grabber.open();
+        
+        robot.structure.kickerSystem.kicker.home();
+        robot.structure.grabber.home();
     }
 
-    /**
-     * This function is called periodically during operator control.
-     */
     @Override
     public void teleopPeriodic() {
-        double throttle = operatorInterface.throttle.read();
-        double wheel = operatorInterface.wheel.read();
-        double driveSpeed = operatorInterface.driveSpeed.read();
-        double turnSpeed = operatorInterface.turnSpeed.read();
+        activePeriodic();
+        
+        double driveSpeed = robot.operator.driveSpeed.read();
+        double turnSpeed = robot.operator.turnSpeed.read()*-1;
 
-        boolean quickTurn = operatorInterface.quickTurn.isTriggered();
-        if(Math.abs(driveSpeed)<0.05)
-            quickTurn = true;
-        systems.drive.cheesy(throttle*0.5, wheel*0.5, quickTurn);
-        if(operatorInterface.writeData.isTriggered())
-            Logger.getInstance().shutdown();
+        // Map throttle from [-1.0, 1.0] to [0.0, 1.0]
+        double throttle = (robot.operator.throttle.read() - 1)/2.0;
+
+        robot.drive.arcade(driveSpeed * throttle, turnSpeed * throttle);
     }
-
-    @Override
-    public void free() {
-        systems.shutdown();
-        super.free();
-    }
-
-    public static Robot getInstance() {
-        return instance;
-    }
-
+        
     public static long time() {
-        return System.nanoTime()-startTime;
+        return System.nanoTime() - START_TIME;
     }
 
     @Deprecated
